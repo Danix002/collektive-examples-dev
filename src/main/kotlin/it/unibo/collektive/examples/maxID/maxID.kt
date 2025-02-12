5571570f4bab50501bf7e4297578c9570ddd4e05
Daniela Magrì <82176325+Danix002@users.noreply.github.com>
2025-02-12 17:50:01 +0100

    Examples in Collektive tutorial: fix

--- src/main/kotlin/it/unibo/collektive/examples/maxID/maxID.kt
+++ src/main/kotlin/it/unibo/collektive/examples/maxID/maxID.kt
@@ -14,43 +14,35 @@ import it.unibo.collektive.alchemist.device.sensors.EnvironmentVariables
  * 4. Assign a distinct color to the nodes with the identified maximum values.
 */
 
-fun Aggregate<Int>.maxID(environment: EnvironmentVariables) =
-    when (environment.get<Boolean>("isMaxID")) {
-        true -> searchMaxNetworkIDValue(environment)
-        false -> environment.get<Int>("maxNetworkID")
-    }
+fun Aggregate<Int>.maxID(environment: EnvironmentVariables) = maxNetworkID(environment)
 
-fun Aggregate<Int>.searchMaxNeighborIDValue(environment: EnvironmentVariables): Int {
+fun Aggregate<Int>.maxNeighborID(): Int {
     // Step 1: Exchange the localId with neighbors and obtain a field of values
     val neighborValues = neighboringViaExchange(local = localId)
 
     // Step 2: Find the maximum value among neighbors (including self)
     val maxValue = neighborValues.max(base = localId)
 
-    // Step 3: Assign the result to a molecule
-    environment.set<Boolean>("isMaxLocalID", localId == maxValue)
-    environment.set<Int>("maxNeighborID", maxValue)
-
     return maxValue
 }
 
-fun Aggregate<Int>.searchMaxNetworkIDValue(environment: EnvironmentVariables): Int {
-    environment.set<Int>("localID", localId)
-
-    when (environment.get<Boolean>("isMaxLocalID")) {
-        true -> searchMaxNeighborIDValue(environment)
-        false -> environment.get<Int>("maxNeighborID")
-    }
-
-    // Step 1: Exchange the maxNeighborID with neighbors and obtain a field of values
-    val neighborValues = neighboringViaExchange(local = environment.get<Int>("maxNeighborID"))
-
-    // Step 2: Find the maximum value among neighbors (including self)
-    val maxValue = neighborValues.max(base = environment.get<Int>("maxNeighborID"))
+fun Aggregate<Int>.maxNetworkID(environment: EnvironmentVariables): Int {
+    val maxLocalValue = maxNeighborID()
 
     // Step 3: Assign the result to a molecule
-    environment.set<Boolean>("isMaxID", localId == maxValue)
-    environment.set<Int>("maxNetworkID", maxValue)
-    
+    environment["isMaxLocalID"] = localId == maxLocalValue
+    environment["localID"] = localId
+    environment["maxNeighborID"] = maxLocalValue
+
+    // Step 1: Exchange the maxNeighborID with neighbors and obtain a field of values
+    val neighborValues = neighboringViaExchange(local = maxLocalValue)
+
+    // Step 2: Find the maximum value among neighbors (including self)
+    val maxValue = neighborValues.max(base = maxLocalValue)
+
+    // Step 3: Assign the result to a molecule (only if using Alchemist)
+    environment["isMaxID"] = localId == maxValue
+    environment["maxNetworkID"] = maxValue
+
     return maxValue
 }
